33b9d09d547431c0e35273bb65920fee839f4b17
garipovagz <garipova.gz@edu.spstu.ru>
2020-04-13 06:42:19 +0300

    new test

--- src/test/java/TailTest.java
+++ src/test/java/TailTest.java
@@ -1,9 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.util.Scanner;
 
 import static org.junit.Assert.*;
@@ -46,5 +44,17 @@ private void AssertFileContent ( String out, String expectedOut) throws FileNotF
         String [] args = {"-c", "5", "-o", "src/files/out1.txt", "src/files/text2.txt" };
         Tail.main(args);
         AssertFileContent( "src/files/expected3.txt" , "src/files/out1.txt");
+    }
+    @Test
+   public void main14() throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        PrintStream old = System.out;
+        PrintStream ps = new PrintStream(baos);
+        System.setOut(ps);
+        String [] args={"-c", "5", "src/files/text1.txt"};
+        Tail.main(args);
+        System.out.flush();
+        System.setOut(old);
+        System.out.println("+"+baos.toString());
     }
 }
